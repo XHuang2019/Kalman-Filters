0267fb07ba8cbfed1b606271ae9f43868333bbdd
Xiaoyu Huang <xiaoyuhuang2019@gmail.com>
2019-01-06 21:05:34 -0800

    V2: reformatted the codes and added some comments

--- KalmanFilter.cpp
+++ KalmanFilter.cpp
@@ -1,14 +1,15 @@
-/**
+/** Discrete-time standard Kalman Filter for simple linear system expressed in state space form
+* @original author: Hayk Martirosyan https://github.com/hmartiro/kalman-cpp.git
+* @revised by Xiaoyu Huang on 2019.01.05
 * Implementation of KalmanFilter class.
-* @original author: Hayk Martirosyan
-* @revised by Xiaoyu Huang
-* @date: 2019.01.05
 */
+
 #include "pch.h"
 #include <iostream>
 #include <stdexcept>
 #include "KalmanFilter.h"
 
+// Constructor 1
 KalmanFilter::KalmanFilter(
     double dt,
     const Eigen::MatrixXd& A,
@@ -20,45 +21,54 @@ KalmanFilter::KalmanFilter(
     m(C.rows()), n(A.rows()), dt(dt), initialized(false),
     I(n, n), x_hat(n), x_hat_new(n)
 {
-  I.setIdentity();
+	I.setIdentity();
 }
 
+// Constructor 2
 KalmanFilter::KalmanFilter() {}
 
 void KalmanFilter::init(double t0, const Eigen::VectorXd& x0) {
-  x_hat = x0;
-  P = P0;
-  this->t0 = t0;
-  t = t0;
-  initialized = true;
+	x_hat = x0;
+	P = P0;
+	this->t0 = t0;
+	t = t0;
+	initialized = true;
 }
 
 void KalmanFilter::init() {
-  x_hat.setZero();
-  P = P0;
-  t0 = 0;
-  t = t0;
-  initialized = true;
+	x_hat.setZero();
+	P = P0;
+	t0 = 0;
+	t = t0;
+	initialized = true;
 }
 
-void KalmanFilter::update(const Eigen::VectorXd& y) {
+void KalmanFilter::update(const Eigen::VectorXd& y) { // is y being updated with a const notation?
 
-  if(!initialized)
-    throw std::runtime_error("Filter is not initialized!");
+	if (!initialized) {
+		throw std::runtime_error("Filter is not initialized!");
+	}
 
-  x_hat_new = A * x_hat;
-  P = A*P*A.transpose() + Q;
-  K = P*C.transpose()*(C*P*C.transpose() + R).inverse();
-  x_hat_new += K * (y - C*x_hat_new);
-  P = (I - K*C)*P;
-  x_hat = x_hat_new;
+	// Time update
+	x_hat_new = A * x_hat; // a priori state estimate; missing B * U
+	P = A * P * A.transpose() + Q; // a priori error covariance estimate
 
-  t += dt;
+	// Measurement update
+	K = P * C.transpose() * (C * P * C.transpose() + R).inverse(); // Kalman gain
+	x_hat_new += K * (y - C*x_hat_new); // a posteriori state estimate
+	P = (I - K*C)*P; // a posteriori error covariance estimate
+
+	// Assign back to x_hat for use in the next instance
+	x_hat = x_hat_new;
+
+	// Record t, mainly used for plotting
+	t += dt;
 }
 
 void KalmanFilter::update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A) {
 
-  this->A = A;
-  this->dt = dt;
-  update(y);
+	// Time-varing A and dt?
+	this->A = A;
+	this->dt = dt;
+	update(y);
 }
--- KalmanFilter.h
+++ KalmanFilter.h
@@ -1,87 +1,72 @@
-/**
-* https://github.com/hmartiro/kalman-cpp.git
-* @original author: Hayk Martirosyan
-* @revised by Xiaoyu Huang
-* @date: 2019.01.05
+/** Discrete-time standard Kalman Filter for simple linear system expressed in state space form
+* @original author: Hayk Martirosyan https://github.com/hmartiro/kalman-cpp.git
+* @revised by Xiaoyu Huang on 2019.01.05
+Future modification:
+1. check observability
+2. add B matrix
 */
 #include <Eigen/Dense>
-
 #pragma once
 
 class KalmanFilter {
+/* Definition of Matrices
+A - State matrix
+B - Input matrix
+C - Output matrix
+Q - Process noise covariance
+R - Measurement noise covariance
+P - Estimate error covariance
+*/
+private:
+
+	// Matrices for computation
+	Eigen::MatrixXd A, C, Q, R, P, K, P0;
+	// System dimensions
+	int n; // number of states
+	int m; // number of measurements
+
+	// Initial and current time
+	double t0, t;
+	// Discrete time step
+	double dt;
+
+	bool initialized;
+
+	// n-size identity (why needed?)
+	Eigen::MatrixXd I;
+
+	// Estimated states
+	Eigen::VectorXd x_hat, x_hat_new;
+
 
 public:
 
-  /**
-  * Create a Kalman filter with the specified matrices.
-  *   A - System dynamics matrix
-  *   C - Output matrix
-  *   Q - Process noise covariance
-  *   R - Measurement noise covariance
-  *   P - Estimate error covariance
-  */
-  KalmanFilter(
-      double dt,
-      const Eigen::MatrixXd& A,
-      const Eigen::MatrixXd& C,
-      const Eigen::MatrixXd& Q,
-      const Eigen::MatrixXd& R,
-      const Eigen::MatrixXd& P
-  );
+	// Constructor 1
+	KalmanFilter(
+		double dt,
+		const Eigen::MatrixXd& A,
+		const Eigen::MatrixXd& C,
+		const Eigen::MatrixXd& Q,
+		const Eigen::MatrixXd& R,
+		const Eigen::MatrixXd& P
+	);
+	
+	// Constructor 2:  blank filter.
+	KalmanFilter();
+	
+	// Initialize the filter with initial states as zero.
+	void init();
 
-  /**
-  * Create a blank estimator.
-  */
-  KalmanFilter();
+	// Initialize the filter with a guess for initial states.
+	void init(double t0, const Eigen::VectorXd& x0);
 
-  /**
-  * Initialize the filter with initial states as zero.
-  */
-  void init();
+	// Update the estimated state based on measured values.The time step is assumed to remain constant.
+	void update(const Eigen::VectorXd& y);
 
-  /**
-  * Initialize the filter with a guess for initial states.
-  */
-  void init(double t0, const Eigen::VectorXd& x0);
+	// Update the estimated state based on measured values using the given time step and dynamics matrix (why needed?)
+	void update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A);
 
-  /**
-  * Update the estimated state based on measured values. The
-  * time step is assumed to remain constant.
-  */
-  void update(const Eigen::VectorXd& y);
-
-  /**
-  * Update the estimated state based on measured values,
-  * using the given time step and dynamics matrix.
-  */
-  void update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A);
-
-  /**
-  * Return the current state and time.
-  */
-  Eigen::VectorXd state() { return x_hat; };
-  double time() { return t; };
-
-private:
-
-  // Matrices for computation
-  Eigen::MatrixXd A, C, Q, R, P, K, P0;
-
-  // System dimensions
-  int m, n;
-
-  // Initial and current time
-  double t0, t;
-
-  // Discrete time step
-  double dt;
-
-  // Is the filter initialized?
-  bool initialized;
-
-  // n-size identity
-  Eigen::MatrixXd I;
-
-  // Estimated states
-  Eigen::VectorXd x_hat, x_hat_new;
+	// Return the current state and time.
+	Eigen::VectorXd state() { return x_hat; };
+	double time() { return t; };
 };
